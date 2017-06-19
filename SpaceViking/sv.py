619f39d1b07bb7119f0da2a03c3743bb90873c0a
maxwellspann <h8uall@gmail.com>
2017-06-19 02:13:53 +0000

    second commit

--- SpaceViking/sv.py
+++ SpaceViking/sv.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python
 from random import randint
 
-
-
-
-
 def attGen():
     return randint(1,6) + randint(1,6)
 
 def hexForm(attrib):
     return str(hex(attrib)[2:].upper())
 
-attributes = []
-
-x = 0 
-while x < 6:
-   attributes.append(attGen())
-   x = x + 1
 
 strng = attGen()
 dex = attGen()
@@ -25,8 +15,7 @@ intel = attGen()
 edu = attGen()
 socSt = attGen()
 
-#print strng
+
 print hexForm(strng) +  hexForm(dex) +  hexForm(endu) +  hexForm(intel) +  hexForm(edu) +  hexForm(socSt)
 
-print ''.join(str(attributes)) 
-#print str(hex(strng + dex + endu + intel + edu + socSt)[2:]).upper() 
+
